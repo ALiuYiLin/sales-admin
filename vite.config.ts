10f204d69f3e8fd3b78b65b0a4a6c748d4e42174
ALiuYiLin <scliuyilin@163.com>
2025-12-25 11:17:36 +0800

    feat: 路径别名配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   css: {
     modules: {
       generateScopedName: mode === 'development'
